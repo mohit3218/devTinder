7893d1590476c8d5aa2965e1157cbe136312e149
Mohit Thakur <techformation.mohit@gmail.com>
2025-07-19 20:00:20 +0530

    Get all the users and get user by search by find

--- src/app.js
+++ src/app.js
@@ -3,20 +3,49 @@ const app = express();
 const port = 8000;
 
 const { connectDB } = require("./config/database");
-const User = require("./models/user")
+const User = require("./models/user");
 
 app.use(express.json());
 
-app.post("/signup" , async (req, res) => {
-    //Creating a new instance of the User model
-    const user = new  User(req.body)
-    try{
-        await user.save();
-        res.send("User added successfully")
-    }catch(err){
-        res.status(400).send(`Something went wrong ${err?.code}`)
+app.post("/signup", async (req, res) => {
+  //Creating a new instance of the User model
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.send("User added successfully");
+  } catch (err) {
+    res.status(400).send(`Something went wrong ${err?.code}`);
+  }
+});
+
+//Get User By E-mail
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  console.log("userEmail ", userEmail);
+  try {
+    const users = await User.find({ emailId: userEmail });
+    if (users.length === 0) {
+      res.status(404).send(`Ussr not found`);
+    } else {
+      res.send(users);
     }
-})
+  } catch (err) {
+    res.status(400).send(`Something went wrong ${err}`);
+  }
+});
+
+app.get("/feed", async (req, res) => {
+  try {
+    const users = await User.find({});
+    if (users.length === 0) {
+      res.status(404).send(`Ussr not found`);
+    } else {
+      res.send(users);
+    }
+  } catch (err) {
+    res.status(400).send(`Something went wrong ${err}`);
+  }
+});
 
 connectDB()
   .then(() => {
@@ -28,5 +57,3 @@ connectDB()
   .catch((err) => {
     console.log("Database cannot be connected!!");
   });
-
-  
