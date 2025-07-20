1b480fb96e0f325150fdc338f4d3cd4867b86247
Mohit Thakur <techformation.mohit@gmail.com>
2025-07-20 18:16:57 +0530

    Pagination added on feed

--- src/routes/user.js
+++ src/routes/user.js
@@ -68,6 +68,11 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
     //3. already sent the connection request
     const loggedInUser = req.user;
 
+    const page = parseInt(req.query.page) || 1;
+    let limit = parseInt(req.query.limit) || 10;
+    limit = limit > 50 ? 50 : limit;
+    const skip = (page - 1) * limit;
+
     const connectionRequests = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
     }).select("fromUserId , toUserId");
@@ -89,7 +94,7 @@ userRouter.get("/user/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hideUsersFromFeed) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select(USER_SAFE_DATA);
+    }).select(USER_SAFE_DATA).skip(skip).limit(limit);
     res.json({ data: users });
   } catch (err) {
     res.status(400).send({ message: err.message });
